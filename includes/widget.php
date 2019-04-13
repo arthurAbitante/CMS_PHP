0586880bd6ec497589e8b3ec1f262706aa950aaf
arthurAbitante <arthur.abitante@gmail.com>
2019-04-13 12:51:27 -0300

    Initialize CMS not complete in Git

---
+++ includes/widget.php
@@ -0,0 +1,5 @@
+<div class="well">
+    <h4>Widget</h4>
+    <p>teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste</p>
+
+</div>
